64d202a7fbffe34488c558a5887d1f63be9c8639
amit haritwal <65955451+competitivewithamit@users.noreply.github.com>
2020-05-27 16:46:33 +0530

    Create 1358A.cpp

---
+++ code forces/c++/1358A.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,a,b) for (ll i = a; i <b; i++)
+#define res(i,a,b) for (ll i = a; i >b; i--)
+#define all(n) n.begin(),n.end()
+typedef long long ll;
+const long long INF = 1e18 + 42;
+#define PB push_back
+#define MP make_pair
+#define F first
+#define S second
+#define SQ(a) (a)*(a)
+typedef vector<ll> vi;
+typedef pair<ll,ll> pi;
+bool comp(int v1,int v2){
+	return v1>v2;
+}
+void sol()
+{
+        cin >> n >> m;
+        cout << (n * m + 1) / 2 << '\n';
+}
+int main()
+{
+#ifndef ONLINE_JUDGE
+	freopen("input.txt","r",stdin);
+	freopen("output.txt","w",stdout);
+#endif
+		int a;
+	cin>>a;
+	while(a--)
+	{
+		sol();
+	}
+}
